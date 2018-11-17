e3627c6944b8aafa4c9dd5fe9fb8962ac2118fc5
Nathan Daniel Peterson <natperson@gmail.com>
2018-11-17 07:22:45 -0800

    Initial commit. Set up docker environment.

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+  fontWeight: 400,
+  fontSize: '1.75rem'
+}
+
+export default () => (
+  <div style={style}>
+    <p>Moving Text</p>
+  </div>
+)
